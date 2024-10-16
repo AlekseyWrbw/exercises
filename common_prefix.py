b58f51c65847ecf351de27513731fc2c677072b0
Alexey Vorobiev <alexey.worobiew@gmail.com>
2024-10-16 23:57:26 +0300

    another solve variant

--- common_prefix.py
+++ common_prefix.py
@@ -3,18 +3,19 @@ Write a function to find the longest common prefix string amongst an array of st
 
 If there is no common prefix, return an empty string "".
 '''
-strs = ["",""]
+strs = ["abra","bbrto", "skib", "ab"]
+#strs = []
 print(len(strs))
-print(strs[0])
+#print(strs[0])
 
 
 class Solution(object):
     def longestCommonPrefix(self, strs):
         strs.sort(key=len)
         common_prefix = []
+        if  len(strs) <=1:
+            return('""')
 
-        if len(strs) == 1:
-            return strs[0]
         for i in range(1, len(strs[0])):
             for k in range(1, len(strs)):
 
@@ -27,3 +28,26 @@ class Solution(object):
                     common_prefix.remove(strs[0][:i])
                     return common_prefix[-1]
 
+solution = Solution()
+
+print(solution.longestCommonPrefix(strs))
+
+
+###еще один вариант
+def longest_common_prefix(strs):
+
+    if not strs:
+        return ""
+
+    min_len = min(len(s) for s in strs)
+    prefix = ""
+    for i in range(min_len):
+        char = strs[0][i]
+        for s in strs[1:]:
+            if s[i] != char:
+                return prefix  # No common prefix
+        prefix += char
+    return prefix
+strings = ["flower", "flow", "flight"]
+
+result = longest_common_prefix(strings)
